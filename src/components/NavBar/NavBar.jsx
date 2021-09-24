4337804a29f74452974921d6bfe7a492da3b8631
StephensIriarte <stephens_iriarte@yahoo.com>
2021-09-24 15:29:58 -0300

    2 Class

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+return (
+
+    <div class="topnav">
+    <a class="active" href="#promociones">Promociones</a>
+    <a href="#snacks">Snacks o Mixs</a>
+    <a href="#frutosSecos">Frutos Secos</a>
+    <a href="#frutasDesi">Frutas Deshidratada</a>
+    <a href="#semillas">Semillas</a>
+    <a href="#chocolates">Chocolates</a>
+  </div>
+)
+
+
+}
+
+export default NavBar
+ 
+  
+  
+
+
