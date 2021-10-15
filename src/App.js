94406b93fac14b4468bc446ff6e8d86d6cd5781d
StephensIriarte <stephens_iriarte@yahoo.com>
2021-10-14 22:59:12 -0300

    class Primera Entrega

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/Main/ItemListContainer';
 import ItemDetailContainer from './components/Main/ItemDetailContainer';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { cartContext } from './context/cartContext';
 
 function App() {
 
   return (
     <div>
-      <BrowserRouter>
-      <NavBar/>
-      <Switch>
-      <Route exact path="/detailProducto/:id">
-        <ItemDetailContainer/>
-      </Route>
-      <Route exact path="/">
-        <ItemListContainer/>
-      </Route>
-      <Route exact path="/Menu">
-        <ItemListContainer/>
-      </Route>
+      <cartContext.Provider>
+        <BrowserRouter>
+        <NavBar/>
+        <Switch>
+        <Route exact path="/detailProducto/:id">
+          <ItemDetailContainer/>
+        </Route>
+        <Route exact path="/">
+          <ItemListContainer/>
+        </Route>
+        <Route exact path="/Menu">
+          <ItemListContainer/>
+        </Route>
 
-      </Switch>
-      </BrowserRouter>
+        </Switch>
+        </BrowserRouter>
+      </cartContext.Provider>
     </div>
   );
 
